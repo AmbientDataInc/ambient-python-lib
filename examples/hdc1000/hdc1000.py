851bc83bfb1916498701b1ec7cf681c8c11df2cc
Takehiko Shimojima <shimojima@gmail.com>
2016-10-12 17:37:34 +0900

    __init__を修正

--- examples/hdc1000/hdc1000.py
+++ examples/hdc1000/hdc1000.py
@@ -10,8 +10,9 @@ class Hdc1000:
 
         self.i2c = wiringpi.I2C() #get I2C
         self.dev = self.i2c.setup(addr) #setup I2C device
+        self.rdy = rdyPin
 
-        wiringpi.pinMode(rdyPin, 0) # set ready pin to INPUT
+        wiringpi.pinMode(self.rdy, 0) # set ready pin to INPUT
 
         self.i2c.writeReg16(self.dev, 0x02, 0x0000) # Config reg を設定 MODE=0 温度、湿度をそれぞれ取り込む
 
@@ -23,7 +24,7 @@ class Hdc1000:
     def getTemp(self):
         self.i2c.writeReg8(self.dev, 0x00, 0x00)
 
-        while wiringpi.digitalRead(7) == 1:
+        while wiringpi.digitalRead(self.rdy) == 1:
             pass
 
         data = struct.unpack('BB', os.read(self.dev, 2))
@@ -34,7 +35,7 @@ class Hdc1000:
     def getHumid(self):
         self.i2c.writeReg8(self.dev, 0x01, 0x00)
 
-        while wiringpi.digitalRead(7) == 1:
+        while wiringpi.digitalRead(self.rdy) == 1:
             pass
 
         data = struct.unpack('BB', os.read(self.dev, 2))
