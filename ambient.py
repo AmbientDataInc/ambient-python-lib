006986d68050483d91c0a22a916fa1c3a05895c8
Takehiko Shimojima <shimojima@gmail.com>
2017-02-19 09:16:40 +0900

    send()の仕様を、1データーでも複数データーの配列でも送れるよう拡張。
    modified:   ambient.py
    modified:   setup.py

--- ambient.py
+++ ambient.py
@@ -4,7 +4,7 @@ import requests
 
 class Ambient:
     def __init__(self, channelId, writeKey, *args):
-        self.url = "http://ambidata.io/api/v2/channels/" + str(channelId) + "/data"
+        self.url = "http://ambidata.io/api/v2/channels/" + str(channelId) + "/dataarray"
         self.writeKey = writeKey
         if len(args) >= 2:
             self.userKey = args[1]
@@ -12,6 +12,9 @@ class Ambient:
             self.readKey = args[0]
 
     def send(self, data):
-        data['writeKey'] = self.writeKey
-        r = requests.post(self.url, json = data)
+        if isinstance(data, list):
+            d = data
+        else:
+            d = [data]
+        r = requests.post(self.url, json = {"writeKey": self.writeKey, "data": d})
         return r
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
         name             = 'ambient',
-        version          = '0.1.0',
+        version          = '0.1.1',
         packages         = find_packages(),
         description      = 'Python module to send data to Ambient',
         license          = 'MIT',
