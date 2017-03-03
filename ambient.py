658607cc1721a99caafa810b96a75c7194898a14
Takehiko Shimojima <shimojima@gmail.com>
2017-03-03 23:01:28 +0900

    Ambientからデーターを読み込む機能を追加
    modified:   README.md
    modified:   ambient.py
    modified:   setup.py

--- ambient.py
+++ ambient.py
@@ -4,7 +4,8 @@ import requests
 
 class Ambient:
     def __init__(self, channelId, writeKey, *args):
-        self.url = "http://ambidata.io/api/v2/channels/" + str(channelId) + "/dataarray"
+        self.url = 'http://ambidata.io/api/v2/channels/' + str(channelId) + '/dataarray'
+        self.channelId = channelId
         self.writeKey = writeKey
         if len(args) >= 2:
             self.userKey = args[1]
@@ -13,8 +14,29 @@ class Ambient:
 
     def send(self, data):
         if isinstance(data, list):
-            d = data
+            __d = data
         else:
-            d = [data]
-        r = requests.post(self.url, json = {"writeKey": self.writeKey, "data": d})
+            __d = [data]
+        r = requests.post(self.url, json = {'writeKey': self.writeKey, 'data': __d})
         return r
+
+    def read(self, **args):
+        url = 'http://ambidata.io/api/v2/channels/' + str(self.channelId) + '/data'
+        __o = []
+        if hasattr(self, 'readKey'):
+            __o.append('readKey=' + self.readKey)
+        if 'date' in args:
+            __o.append('date=' + args['date'])
+        else:
+            if 'start' in args and 'end' in args:
+                __o.append('start=' + args['start'])
+                __o.append('end=' + args['end'])
+            else:
+                if 'n' in args:
+                    __o.append('n=' + str(args['n']))
+                    if 'skip' in args:
+                        __o.append('skip=' + str(args['skip']))
+        if len(__o) > 0:
+            url = url + '?' + '&'.join(__o)
+        self.r = requests.get(url)
+        return list(reversed(self.r.json()))
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
         name             = 'ambient',
-        version          = '0.1.1',
+        version          = '0.1.2',
         packages         = find_packages(),
         description      = 'Python module to send data to Ambient',
         license          = 'MIT',
